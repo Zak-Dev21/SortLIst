922236f6cf05e0df259e1ba94f381cf98ea28373
Zak <zakariabouaddou@gmail.com>
2024-10-07 16:17:56 -0700

    fixed header for proper syntax and adjust logic to (i swaps with i + 1 instead of i swaps with i - 1)

--- Header.h
+++ Header.h
@@ -2,18 +2,20 @@
 #define Header_h
 
 int temp = 0;
-int list[4] = {6,2,5,1}
+int list[4] = { 6,2,5,1 };
 
-int BubbleSort(list[]) {
-	for (int j = 1; j < 4; j++) {
-		for (int i = 2; i < 4; i++) {
-			if (list[i - 1] > list[i]) {
-				temp = list[i - 1];
-				list[i - 1] = list[i];
-				list[i] = temp;
-			}
-		}
-	}
+void BubbleSort(int list[], int size) {
+    int temp;
+    for (int j = 0; j < size - 1; j++) {
+        for (int i = 0; i < size - j - 1; i++) {
+            if (list[i] > list[i + 1]) {
+                // Swap elements
+                temp = list[i];
+                list[i] = list[i + 1];
+                list[i + 1] = temp;
+            }
+        }
+    }
 }
 
 
--- SortLIst.cpp
+++ SortLIst.cpp
@@ -1,9 +1,20 @@
 
 
 #include <iostream>
+#include "Header.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int list[4] = { 6, 2, 5, 1 };
+    int size = sizeof(list) / sizeof(list[0]);
+
+    BubbleSort(list, size);
+
+    // Print sorted list
+    for (int i = 0; i < size; i++) {
+        std::cout << list[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
 
